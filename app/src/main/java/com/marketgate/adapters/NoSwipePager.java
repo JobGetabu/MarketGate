b379847271d5689b96fb72f38b7d04c3900c19bf
Job Getabu <getabujob@gmail.com>
2019-02-25 23:42:29 +0300

    added pager and adapter

---
+++ app/src/main/java/com/marketgate/adapters/NoSwipePager.java
@@ -0,0 +1,44 @@
+package com.marketgate.adapters;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.viewpager.widget.ViewPager;
+
+/**
+ * Created by Job on Thursday : 4/12/2018.
+ */
+
+//Swiping must be disabled. for the bottom nav... as we are using a view page
+//NoSwipePager in your XML layout instead of a regular ViewPager
+
+
+public class NoSwipePager extends ViewPager {
+    private boolean enabled;
+
+    public NoSwipePager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
