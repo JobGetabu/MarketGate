e355433f91d3e56829340d890eacaa815f31889c
Job Getabu <getabujob@gmail.com>
2019-03-05 17:14:10 +0300

    profile for farmer

--- app/src/main/java/com/marketgate/farmer/FarmProfile.kt
+++ app/src/main/java/com/marketgate/farmer/FarmProfile.kt
@@ -6,18 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.marketgate.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.marketgate.models.USER_FARMER
+import com.marketgate.models.UserFarmer
+import com.marketgate.utils.showShortSnackbar
+import com.raiachat.util.loadUrl
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
  *
  */
+
+
 class FarmProfile : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +32,49 @@ class FarmProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //firebase
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        setUI()
+
+        profile_btn_update.setOnClickListener { updateUser() }
+    }
+
+    private fun setUI() {
+        val user = auth.currentUser
+
+        firestore.collection(USER_FARMER).document(user!!.uid).get()
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                    val userFarmer  = it.result!!.toObject(UserFarmer::class.java)
+
+                    profile_image.loadUrl(userFarmer?.photourl)
+                    profile_fullname.editText!!.setText( userFarmer!!.name)
+                    profile_coop.editText!!.setText( userFarmer.cooperativename)
+                    profile_location.editText!!.setText( userFarmer.locationstring)
+                    profile_selling.isChecked = userFarmer.sellingstatus
+                }
+            }
+    }
+
+    private fun updateUser() {
+        val user = auth.currentUser
+
+        firestore.collection(USER_FARMER).document(user!!.uid)
+            .update("name",profile_fullname.editText!!.text,
+                "sellingstatus",profile_selling.isChecked,
+                "locationstring",profile_location.editText!!.text,
+            "cooperativename",profile_coop.editText!!.text)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+                   showShortSnackbar("Profile updated",farmprofile_main)
+                }
+            }
     }
 
 
