c10fae52564a1db09585e20aa33c30b3bcf6360b
Job Getabu <getabujob@gmail.com>
2019-03-11 15:54:47 +0300

    did on boarding for marketgate

--- app/src/main/java/com/marketgate/core/SplashActivity.java
+++ app/src/main/java/com/marketgate/core/SplashActivity.java
@@ -1,6 +1,5 @@
 package com.marketgate.core;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,28 +10,21 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
-       /* requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);*/
 
-        String name = getApplicationContext().getPackageName();
-        SharedPreferences settings = getSharedPreferences(name, Context.MODE_PRIVATE);
+        Intent i = null;
 
-        boolean firstRun = settings.getBoolean("firstRun", true);
+        SharedPreferences pref = getSharedPreferences(getApplicationContext().getPackageName(), MODE_PRIVATE);
 
-        if (firstRun)//if running for first time
-        //Intro will load for first time
-        {
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putBoolean("firstRun", false);
-            editor.apply();
-
+        if (pref.getBoolean("isFirstTimeLaunch", true)) {
+            i = new Intent(SplashActivity.this, IntroActivity.class);//introActivity
+            startActivity(i);
+            finish();
+        } else {
+            i = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
         }
 
-        //Intent i = new Intent(SplashActivity.this, FarmerActivity.class);//introActivity
-        Intent i = new Intent(SplashActivity.this, LoginActivity.class);//introActivity
-        startActivity(i);
-        finish();
+
     }
 }
