040b71493d532392bc0f42d33ef0384991959355
Job Getabu <getabujob@gmail.com>
2019-03-12 21:52:44 +0300

    new login screen

---
+++ app/src/main/java/com/marketgate/core/SigninActivity.kt
@@ -0,0 +1,11 @@
+package com.marketgate.core
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SigninActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
